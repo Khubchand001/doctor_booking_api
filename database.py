bb2a935956920b9e06e36723296f470948a86cf9
khubchand <khubchanddewangan32@gmail.com>
2026-04-13 16:29:59 +0530

    feat: initialize Alembic database migrations and update connection string to PostgreSQL

--- database.py
+++ database.py
@@ -6,13 +6,20 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 # Load environment variables
 load_dotenv()
 
-DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./doctor.db")
+DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://doctordb_ct2n_user:bY3HOuDV9tMzYwwrX5H8HJ18nu4UogpU@dpg-d7eclf9j2pic73855810-a/doctordb_ct2n")
+# Render provides "postgres://", but SQLAlchemy requires "postgresql://"
+if DATABASE_URL.startswith("postgres://"):
+    DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
+
 SECRET_KEY = os.getenv("SECRET_KEY", "secret123")
 ALGORITHM = os.getenv("ALGORITHM", "HS256")
 
+# Connect args (only for SQLite)
+connect_args = {"check_same_thread": False} if DATABASE_URL.startswith("sqlite") else {}
+
 engine = create_engine(
     DATABASE_URL,
-    connect_args={"check_same_thread": False}
+    connect_args=connect_args
 )
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
